92f54c0af83565feae698f29b0cd015d568baf3b
vihaannigam104 <83755612+vihaannigam104@users.noreply.github.com>
2022-06-24 12:27:11 +0530

    Update sketch.js

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -1,42 +1,80 @@
+var bg,sleep, brush, gym, eat, bath, move;
+var astronaut;
 
-//declaring variables
-var astronaut,sleep,brush,gym,eat,drink,move;
-
-function prelaod(){
-  //loading images and animations 
-   bg =loadImage("iss.png");
-   sleep = loadAnimation("sleep.png");
-   brush = loadAnimation("brush.png");
-   gym = loadAnimation("gym1.png","gym2.png","gym11.png","gym12.png");
-   eat = loadAnimation("eat1.png","eat2.png");
-   drink = loadAnimation("drink1.png","drink2.png");
-   move = loadAnimation("move.png","move1.png");
+function preload(){
+  bg= loadImage("images/iss.png");
+  sleep = loadAnimation("images/sleep.png");
+  brush = loadAnimation("images/brush.png");
+  gym = loadAnimation("images/gym1.png","images/gym1.png","images/gym2.png","images/gym2.png");
+  eat = loadAnimation("images/eat1.png","images/eat1.png","images/eat1.png","images/eat2.png","images/eat2.png","images/eat2.png");
+  bath = loadAnimation("images/bath1.png","images/bath1.png","images/bath1.png","images/bath2.png","images/bath2.png","images/bath2.png");
+ move = loadAnimation("images/move1.png","images/move1.png","images/move.png","images/move.png");
 }
 
 function setup() {
-  createCanvas(800,400);
-
-  astronaut= createSprite(300,200);
-  astronaut.addAnimation("sleeping" , sleep);
+  createCanvas(600, 500);
+  
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
   astronaut.scale = 0.1;
   
 }
 
 function draw() {
-  
   background(bg);
+  drawSprites();
+
+  textSize(20);
+  fill("white")
+  text("Instructions:",20, 35);
+  textSize(15);
+  text("Up Arrow = Brushing",20, 55);
+  text("Down Arrow = Gymming",20, 70);
+  text("Left Arrow = Eating",20, 85);
+  text("Right Arrow = Bathing",20, 100);
+  text("m key = Moving",20, 115);
   
+  edges=createEdgeSprites();
+  astronaut.bounceOff(edges);
   
-  if(keyDown("UP_ARROW")) {
+  if(keyDown("UP_ARROW")){
     astronaut.addAnimation("brushing", brush);
     astronaut.changeAnimation("brushing");
     astronaut.y = 350;
     astronaut.velocityX = 0;
-    astronaut.velocityY = 0; 
+    astronaut.velocityY = 0;
   }
   
+  if(keyDown("DOWN_ARROW")){
+    astronaut.addAnimation("gymming", gym);
+    astronaut.changeAnimation("gymming");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
+  
+  if(keyDown("LEFT_ARROW")){
+    astronaut.addAnimation("eating", eat);
+    astronaut.changeAnimation("eating");
+    astronaut.x = 150;
+    astronaut.y = 350;
+    astronaut.velocityX = 0.5;
+    astronaut.velocityY = 0.5;
+  }
+  
+  if(keyDown("RIGHT_ARROW")){
+    astronaut.addAnimation("bathing", bath);
+    astronaut.changeAnimation("bathing");
+    astronaut.x = 300;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
+  }
 
-  
-  
-  drawSprites();
+  if(keyDown("m")){
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+  }
+
 }
